500a5fcc53381bcabb31e72de6ba2b51492040ff
Dāniels Dūda 221RDB081 <124591745+DanielsDuda221RDB081@users.noreply.github.com>
2023-02-20 22:06:17 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,30 +1,66 @@
 # python3
-
+import numpy
 import sys
 import threading
 
 
+
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    arr = numpy.array(range(n))
+    parents = parents.split()
+    parents = map(int, parents)
+    parents = list(parents)
+    s=0
+    x=0
+    height=1
+    max_height=0
+    for i in parents:
+        arr[x] = i
+        x=x+1
+    x=0
+    
+    
+    
+
+    while(True):
+        x=arr[x]
+        
+        if s==(n-1):
+            max_height=max(max_height, height)
+            return max_height
+        elif x==-1:
+            s=s+1
+            max_height=max(max_height, height)
+            height = 1
+            x=s
+            continue
+        
+        else:
+            height = height +1
+            
+
+
+
+            
+
+
 
 
 def main():
-    # implement input form keyboard and from files
+    text = input()
+    if "I" in text:
+        n= int(input())
+    elif "F" in text:
+        pass
+    else:
+        return()
+    parents= input()
+    result = compute_height(n, parents)
+    print(result)
+
+main()
     
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
 
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+
+
